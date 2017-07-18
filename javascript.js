92399c7a6186ced28fa7a226ae48a5d9b8574f45
phbalanced <pascal.hargrove@gmail.com>
2017-07-17 20:01:56 -0500

    PreWork Module 10, Assn 10 - JavaScript
    Practicing Javascript

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+document.getElementById("growBtn").addEventListener("click", function(){
+document.getElementById("box").style.height = "250px";
+});
+
+document.getElementById("blueBtn").addEventListener("click", function(){
+document.getElementById("box").style.backgroundColor = "blue";
+});
+document.getElementById("fadeBtn").addEventListener("click", function(){
+document.getElementById("box").style.opacity = "0.5";
+});
+document.getElementById("resetBtn").addEventListener("click", function(){
+document.getElementById("box").style.height = "150px";
+document.getElementById("box").style.backgroundColor = "orange";
+document.getElementById("box").style.opacity = "1.0";
+});
